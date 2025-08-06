049180ac2d5949a07dcff8914910256707927a31
tkrmagid <tkrmagid@gmail.com>
2025-08-06 07:58:26 +0000

    수정

--- ytm/src/index.ts
+++ ytm/src/index.ts
@@ -14,3 +14,10 @@ import { getPlaylist } from "./core/getPlaylist";
   console.log(playlist);
   console.log(playlist.videos[0]);
 })();
+
+
+// 배포용 코드
+export * from "./core/getChannel";
+export * from "./core/getPlaylist";
+export * from "./core/getSearch";
+export * from "./core/getVideo";
